9501646282436a271ab41c153464cc8a7254f3ab
Lyz Coote <44366896+lyzcoote@users.noreply.github.com>
2024-11-13 15:03:01 +0100

    Added generate script for powershell

--- generate.ps1
+++ generate.ps1
@@ -1,8 +1,25 @@
-# Scarica il file openapi.yaml
 Invoke-WebRequest -Uri "https://vrchatapi.github.io/specification/openapi.yaml" -OutFile "openapi.yaml"
 
-# Installa openapi-codegen
 go install github.com/mayocream/openapi-codegen@latest
 
-# Genera il codice utilizzando openapi-codegen
 openapi-codegen -i ./openapi.yaml -o . -p vrchat
+
+# Added UserAgent to NewClient function, otherwise it will return 403
+(Get-Content ./client.gen.go) -replace 'func NewClient\(baseURL string\)', 'func NewClient(baseURL string, UserAgent string)' | Set-Content ./client.gen.go
+(Get-Content ./client.gen.go) -replace 'client: resty.New\(\).SetBaseURL\(baseURL\),', 'client: resty.New().SetBaseURL(baseURL).SetHeader("User-Agent", UserAgent),' | Set-Content ./client.gen.go
+
+# Import net/http for the cookie related functions below
+(Get-Content ./client.gen.go) -replace '^import \(', 'import (    
+    "net/http"' | Set-Content ./client.gen.go
+
+# Added SetCookie and GetCookies function to easily manage cookies
+Add-Content -Path ./client.gen.go -Value @"
+
+func (c *Client) SetCookie(cookie *http.Cookie) {
+    c.client.SetCookie(cookie)
+}
+
+func (c *Client) GetCookies() []*http.Cookie {
+    return c.client.Cookies
+}
+"@
