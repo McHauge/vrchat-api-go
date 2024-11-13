c98323c1e28b35f47c30aa8264dfccf71d9745bc
Lyz Coote <44366896+lyzcoote@users.noreply.github.com>
2024-11-13 20:52:10 +0100

    Updated example

--- examples/main.go
+++ examples/main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"github.com/lyzcoote/vrchat-api-go"
+	"log"
 )
 
 func main() {
 	client := vrchat.NewClient("https://api.vrchat.cloud/api/1", "MyApp-Test/1.0")
 
-	err := client.AuthenticateTOTP("username", "password", "totp")
+	resp, err := client.Authenticate("username", "password")
 	if err != nil {
 		panic(err)
+	}
+
+	if resp == "{\"requiresTwoFactorAuth\":[\"totp\",\"otp\"]}" {
+		resp, err = client.VerifyTOTP("username", "password", "123456")
+		if err != nil {
+			panic(err)
+		}
+		log.Println(resp)
 	}
 
 	user, err := client.GetCurrentUser()
