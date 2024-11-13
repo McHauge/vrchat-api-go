7250cf987f756c9dd39c030749f9d351eea1d2ab
Lyz Coote <44366896+lyzcoote@users.noreply.github.com>
2024-11-13 14:58:54 +0100

    Add email and recovery OTP authentication methods to VRChat API client

--- auth.go
+++ auth.go
@@ -4,10 +4,61 @@ import (
 	"fmt"
 )
 
-// Authenticate authenticates the client with the VRChat API
-func (c *Client) Authenticate(username, password, totp string) error {
+// AuthenticateRecoveryOTP authenticates the client with the VRChat API using the email recovery OTP code.
+func (c *Client) AuthenticateRecoveryOTP(username, password, totp string) error {
 	c.client.SetHeaders(map[string]string{
-		"User-Agent":   "vrchat-go/0.0.0 mayo@linux.com",
+		"Accept":       "application/json",
+		"Content-Type": "application/json",
+	})
+	resp, err := c.client.R().
+		SetBasicAuth(username, password).
+		SetBody(map[string]string{
+			"code": totp,
+		}).
+		Post("/auth/twofactorauth/otp/verify")
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode() != 200 {
+		return fmt.Errorf("failed to authenticate: %s", resp.String())
+	}
+
+	cookies := resp.Cookies()
+	c.client.SetCookies(cookies)
+
+	return nil
+}
+
+// AuthenticateEmailOTP authenticates the client with the VRChat API using the email OTP code.
+func (c *Client) AuthenticateEmailOTP(username, password, totp string) error {
+	c.client.SetHeaders(map[string]string{
+		"Accept":       "application/json",
+		"Content-Type": "application/json",
+	})
+	resp, err := c.client.R().
+		SetBasicAuth(username, password).
+		SetBody(map[string]string{
+			"code": totp,
+		}).
+		Post("/auth/twofactorauth/emailotp/verify")
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode() != 200 {
+		return fmt.Errorf("failed to authenticate: %s", resp.String())
+	}
+
+	cookies := resp.Cookies()
+	c.client.SetCookies(cookies)
+
+	return nil
+}
+
+// AuthenticateTOTP authenticates the client with the VRChat API using the TOTP code.
+func (c *Client) AuthenticateTOTP(username, password, totp string) error {
+	c.client.SetHeaders(map[string]string{
 		"Accept":       "application/json",
 		"Content-Type": "application/json",
 	})
